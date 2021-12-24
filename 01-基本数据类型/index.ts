ade0bd18e8001e244978cb9e9409dd02f10dfd9b
张子豪 <1844023095@qq.com>
2021-12-25 02:04:02 +0800

    基本类型

---
+++ 01-基本数据类型/index.ts
@@ -0,0 +1,42 @@
+let flag : boolean ;
+flag = true
+flag = null
+flag = undefined
+flag = Boolean(1)
+
+let str : string;
+str = ""
+str = null
+str = undefined
+str = String(123)
+
+let num : number
+num = 1
+num = Number ('12')
+num = null
+num = undefined
+
+let kong : null;
+kong = undefined
+kong = null 
+
+let und : undefined
+und = undefined
+und = null
+
+let vo : void
+vo = null
+vo = undefined
+
+function testvoid () : void{
+  return null 
+}
+
+function testvoid1 () : void{
+  return undefined 
+}
+
+function testvoid2 () : void{
+  alert('haha ') ;
+}
+
---
+++ 02-任意类型/index.ts
@@ -0,0 +1,13 @@
+let what : any
+what = 123
+what = true
+what = "haha"
+function test ( ) : any{
+  return "124"
+}
+function test1 ( ) : any{
+  return true
+}
+function test2 ( ) : any{
+  return Math.min
+}
